1c080098cfb5719d60abb519db76bc39f3cf7d83
az0319h <az0319h@naver.com>
2025-09-10 19:20:11 +0900

    refactor: 홈 화면 프로젝트 섹션 구조 및 다국어 키 개편

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,3 @@
-import { readAllProjects } from "@/api/readAllProjects.action";
 import AppLink from "@/components/common/AppLink";
 import ProjectCard from "@/components/common/ProjectCard";
 import HeroBanner from "@/components/domain/home/HeroBanner";
@@ -6,26 +5,33 @@ import Image from "next/image";
 import blog from "@/assets/images/blog.png";
 import { readDeployProjectUrl } from "@/api/raedDeployProjectUrl.action";
 import { getTranslations } from "next-intl/server";
+import { readTeamProjects } from "@/api/readTeamProjects.action";
+import { readPersonalProjects } from "@/api/readPersonalProjects.action";
+import { readLatestProject } from "@/api/readLatestProject.action";
 
 export default async function HomePage() {
   const t = await getTranslations("HomePage");
-  const [datas, deployUrl] = await Promise.all([
-    readAllProjects(),
-    readDeployProjectUrl(),
-  ]);
+
+  const [latestProject, teamProjects, personalProjects, deployUrl] =
+    await Promise.all([
+      readLatestProject(),
+      readTeamProjects(),
+      readPersonalProjects(),
+      readDeployProjectUrl(),
+    ]);
 
   return (
     <div>
       <h1 className="text-center mb-3 md:mb-4   text-14-semibold md:text-16-semibold">
         {t("title")}
       </h1>
-      <HeroBanner data={datas[0]} />
+      {latestProject && <HeroBanner data={latestProject} />}
       <div>
-        <div className="relative z-10 py-14">
-          <AppLink text={t("appLink")} href="/projects/" />
+        <div className="relative z-10 py-14  md:py-16">
+          <AppLink text={t("teamProjectLink")} href="/projects/?tab=2" />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-14">
-          {datas.slice(1).map((data, idx) => (
+          {teamProjects.map((data, idx) => (
             <div
               key={data.id}
               className={`
@@ -33,11 +39,27 @@ export default async function HomePage() {
         ${idx >= 3 ? "lg:hidden" : ""} /* lg 이상에서는 3개까지만 */
       `}
             >
-              <ProjectCard data={data} currentTab="1" />
+              <ProjectCard data={data} currentTab="2" />
             </div>
           ))}
         </div>
-        <div className="relative z-10 pt-14 md:py-14 flex justify-center">
+        <div className="relative z-10 py-14 md:py-16">
+          <AppLink text={t("personalProjectLink")} href="/projects/?tab=3" />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-14">
+          {personalProjects.map((data, idx) => (
+            <div
+              key={data.id}
+              className={`
+        ${idx >= 4 ? "hidden" : ""}   /* 기본적으로 4개까지만 */
+        ${idx >= 3 ? "lg:hidden" : ""} /* lg 이상에서는 3개까지만 */
+      `}
+            >
+              <ProjectCard data={data} currentTab="3" />
+            </div>
+          ))}
+        </div>
+        <div className="relative z-10 pt-14 md:py-16 flex justify-center">
           <AppLink text={t("more")} href="/projects/" />
         </div>
       </div>
